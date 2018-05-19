57ceec2aaa183ebb365e0b7b5486ef1e1f195e8c
Xiaming Chen <chen_xm@outlook.com>
2018-05-19 15:55:58 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name="flex_version",
+    version='1.0.1',
+    url='https://github.com/caesar0301/FlexVersion',
+    author='Xiaming Chen',
+    author_email='chenxm35@gmail.com',
+    description='A cute Python library to manipulate version stuff.',
+    license="Apache License, Version 2.0",
+    packages=['flex_version'],
+    keywords=['utility', 'versioning'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
