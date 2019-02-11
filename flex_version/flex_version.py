2013e5dcaf74cb46bc266558ab129b4ec549d490
Xiaming Chen <xiaming.chen@transwarp.io>
2019-02-11 15:29:00 +0800

    Support version delta

--- flex_version/flex_version.py
+++ flex_version/flex_version.py
@@ -6,6 +6,15 @@ import re
 __all__ = ['FlexVersion', 'VersionMeta', 'VersionDelta']
 
 
+def _cmp(x, y):
+    return 0 if x == y else 1 if x > y else -1
+
+
+def _cmperror(x, y):
+    raise TypeError("can't compare '%s' to '%s'" % (
+                    type(x).__name__, type(y).__name__))
+
+
 class VersionMeta(object):
     """
     A meta representation of version control, e.g., prefix-x.y.z.b-suffix
@@ -20,70 +29,107 @@ class VersionMeta(object):
     """
 
     _v_regex = r"(?P<prefix>.*\-)?(?P<major>\d+)(?P<minor>\.\d+)" \
-        + r"(?P<maintenance>\.\d+)?(?P<build>\.\d+)?(?P<suffix>\-.*)?"
+        + r"(?P<maintenance>\.\d+)?(?P<build>\.\d+)?(?P<suffix_raw>\-.*)?"
+    _suffix_regex = '(?P<suffix>[^\d]*)(?P<version>\d+)?'
 
-    _suffix_regex = '.*(?P<suffix_version>\d+)'
-    _suffix_version_sig = 'suffix_version'
-
-    def _trim_field(self, field, chars=' \r\n-.'):
-        if field is not None:
-            field = field.strip(chars)
-        return field
-
-    def __init__(self, version_str, versioned_suffix=None):
+    def __init__(self, version_str):
         self._raw = version_str
         self.prefix = None
         self.major = None
         self.minor = None
         self.maintenance = None
         self.build = None
+        self._suffix_raw = None
         self.suffix = None
         self.suffix_version = None
 
-        versioned_suffix = versioned_suffix if versioned_suffix else self._suffix_regex
+        def _trim_field(field, chars=' \r\n-.'):
+            if field is not None:
+                field = field.strip(chars)
+            return field
 
         matched = re.match(self._v_regex, version_str)
         if matched:
-            self.prefix = self._trim_field(matched.group('prefix'))
-            self.major = int(self._trim_field(matched.group('major')))
+            self.prefix = _trim_field(matched.group('prefix'))
+            self.major = int(_trim_field(matched.group('major')))
 
-            _minor = self._trim_field(matched.group('minor'))
+            _minor = _trim_field(matched.group('minor'))
             if _minor is not None:
                 self.minor = int(_minor)
 
-            _maintenance = self._trim_field(matched.group('maintenance'))
+            _maintenance = _trim_field(matched.group('maintenance'))
             if _maintenance is not None:
                 self.maintenance = int(_maintenance)
 
-            _build = self._trim_field(matched.group('build'))
+            _build = _trim_field(matched.group('build'))
             if _build is not None:
                 self.build = int(_build)
 
-            self.suffix = self._trim_field(matched.group('suffix'))
+            self._suffix_raw = _trim_field(matched.group('suffix_raw'))
         else:
             raise ValueError(
                 'Could not parse the given version: {}'.format(version_str))
 
         # Suuport versioned suffix: whose pattern can be specified.
-        if self.suffix is not None and versioned_suffix:
-            compiled = re.compile(versioned_suffix)
-            matched = compiled.match(self.suffix)
+        if self._suffix_raw:
+            compiled = re.compile(self._suffix_regex)
+            matched = compiled.match(self._suffix_raw)
             if matched:
-                if self._suffix_version_sig not in matched.groupdict():
-                    raise ValueError('A defaut group "{}" should be specified when enable versioned suffix'
-                                     .format(self._suffix_version_sig))
-                self.suffix_version = int(self._trim_field(
-                    matched.group(self._suffix_version_sig)))
+                self.suffix = _trim_field(matched.group('suffix'))
+                suffix_version = matched.group('version')
+                if suffix_version is not None:
+                    self.suffix_version = int(suffix_version)
 
     def __str__(self):
         return self._raw
 
+    # Comparisons of VersionMeta objects with other.
+
+    def __eq__(self, other):
+        if isinstance(other, VersionMeta):
+            return (self - other) == VersionDelta.zero
+        else:
+            return False
+
+    def __le__(self, other):
+        if isinstance(other, VersionMeta):
+            return (self - other) <= VersionDelta.zero
+        else:
+            _cmperror(self, other)
+
+    def __lt__(self, other):
+        if isinstance(other, VersionMeta):
+            return (self - other) < VersionDelta.zero
+        else:
+            _cmperror(self, other)
+
+    def __ge__(self, other):
+        if isinstance(other, VersionMeta):
+            return (self - other) >= VersionDelta.zero
+        else:
+            _cmperror(self, other)
+
+    def __gt__(self, other):
+        if isinstance(other, VersionMeta):
+            return (self - other) > VersionDelta.zero
+        else:
+            _cmperror(self, other)
+
     def __sub__(self, other):
         assert isinstance(other, VersionMeta)
 
+        if self.prefix != other.prefix:
+            raise ValueError('VersionMeta substibution requires the same prefix: {} vs. {}'.format(
+                self.prefix, other.prefix))
+
+        if self.suffix != other.suffix:
+            raise ValueError('VersionMeta substibution requires the same suffix: {} vs. {}'.format(
+                self.suffix, other.suffix))
+
         def _verdiff(v1, v2):
             if None in (v1, v2):
-                return None
+                # The difference from None is zero
+                return 0
             return v1 - v2
 
         return VersionDelta(
@@ -93,11 +139,6 @@ class VersionMeta(object):
             build=_verdiff(self.build, other.build),
             sver=_verdiff(self.suffix_version, other.suffix_version)
         )
-
-
-def _cmperror(x, y):
-    raise TypeError("can't compare '%s' to '%s'" % (
-                    type(x).__name__, type(y).__name__))
 
 
 class VersionDelta(object):
@@ -130,15 +171,15 @@ class VersionDelta(object):
 
     def __repr__(self):
         args = []
-        if self._major:
+        if self._major is not None:
             args.append("major=%d" % self._major)
-        if self._minor:
+        if self._minor is not None:
             args.append("minor=%d" % self._minor)
-        if self._maintenance:
+        if self._maintenance is not None:
             args.append("maintenance=%d" % self._maintenance)
-        if self._build:
+        if self._build is not None:
             args.append("build=%d" % self._build)
-        if self._sver:
+        if self._sver is not None:
             args.append("sver=%d" % self._sver)
         if not args:
             args.append('0')
@@ -294,6 +335,7 @@ VersionDelta.max = VersionDelta(
     build=999999999,
     sver=999999999
 )
+VersionDelta.zero = VersionDelta(0, 0, 0, 0, 0)
 
 
 class FlexVersion(object):
@@ -341,25 +383,6 @@ class FlexVersion(object):
         return False
 
     @classmethod
-    def _cmp_ver_num(cls, n1, n2, default=None, delta=False):
-        """ Compare version numbers
-        """
-        if None in (n1, n2):
-            return default
-
-        diff = n1 - n2
-        if delta is True:
-            return diff
-
-        # Normalization
-        if diff < 0:
-            return cls.VERSION_LESS_THAN
-        elif diff == 0:
-            return cls.VERSION_EQUAL
-        else:
-            return cls.VERSION_BIGGER_THAN
-
-    @classmethod
     def compares(cls, v1, v2, match_prefix=True, compare_suffix_version=True):
         """
         Compare the level of two versions.
@@ -379,30 +402,20 @@ class FlexVersion(object):
             raise ValueError('The compared versions should take the same prefix: {} vs. {}.'
                              .format(v1.prefix, v2.prefix))
 
-        res = cls._cmp_ver_num(v1.major, v2.major)
-        if res != cls.VERSION_EQUAL:
-            return res
+        # if compare_suffix_version and v1.suffix != v2.suffix:
+        #     raise ValueError('The compared versions should take the same suffix: {} vs. {}.'
+        #                      .format(v1.suffix, v2.suffix))
 
-        res = cls._cmp_ver_num(v1.minor, v2.minor, default=cls.VERSION_EQUAL)
-        if res != cls.VERSION_EQUAL:
-            return res
+        delta = v1 - v2
+        if not compare_suffix_version:
+            delta.sver = 0
 
-        res = cls._cmp_ver_num(
-            v1.maintenance, v2.maintenance, default=cls.VERSION_EQUAL)
-        if res != cls.VERSION_EQUAL:
-            return res
-
-        res = cls._cmp_ver_num(v1.build, v2.build, default=cls.VERSION_EQUAL)
-        if res != cls.VERSION_EQUAL:
-            return res
-
-        if compare_suffix_version:
-            res = cls._cmp_ver_num(
-                v1.suffix_version, v2.suffix_version, default=cls.VERSION_EQUAL)
-            if res != cls.VERSION_EQUAL:
-                return res
-
-        return cls.VERSION_EQUAL
+        if delta == VersionDelta.zero:
+            return cls.VERSION_EQUAL
+        elif delta > VersionDelta.zero:
+            return cls.VERSION_BIGGER_THAN
+        else:
+            return cls.VERSION_LESS_THAN
 
     @classmethod
     def shares_prefix(cls, v1, v2):
@@ -416,6 +429,9 @@ class FlexVersion(object):
 
 
 if __name__ == '__main__':
+    
+    # VersionMeta parsers
+
     v = 'flexver-1.0.0-rc1'
     vm = VersionMeta(v)
     assert vm.prefix == 'flexver'
@@ -423,7 +439,7 @@ if __name__ == '__main__':
     assert vm.minor == 0
     assert vm.maintenance == 0
     assert vm.build is None
-    assert vm.suffix == 'rc1'
+    assert vm.suffix == 'rc'
     assert vm.suffix_version == 1
 
     v = 'flexver-1.0.0-final'
@@ -449,17 +465,33 @@ if __name__ == '__main__':
     assert vm.major == 1
     assert vm.minor == 0
 
+    # VersionMeta substitution
+
     v1 = VersionMeta('flexver-1.0.0-rc0')
     v2 = VersionMeta('flexver-1.0.0-rc4')
     v3 = VersionMeta('flexver-1.0.0-final')
     v4 = VersionMeta('flexver-1.1.0-rc0')
     v5 = VersionMeta('flexver-1.1.0-final')
-    print(v1 - v2)
+
+    # VersionMeta comparisons
+
+    assert v1 < v2
+    assert v1 <= v2
+    try:
+        assert v1 < v3
+    except ValueError as e:
+        print('INFO: Caught error: %s' % e)
+    assert v3 == v3
+    assert v5 >= v3
+    try:
+        assert v5 > v1
+    except ValueError as e:
+        print('INFO: Caught error: %s' % e)
 
     try:
         FlexVersion.compares('flexver-1.0', 'other-1.0')
     except ValueError as e:
-        print('INFO: Caught error: {}'.format(e))
+        print('INFO: Caught error: %s' % e)
 
     assert FlexVersion.compares('flexver-1.0', 'flexver-1.0') == 0
     assert FlexVersion.compares('flexver-1.0', 'flexver-2.0') < 0
